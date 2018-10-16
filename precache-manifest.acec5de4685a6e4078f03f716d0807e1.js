f605e227eed4f6fbbc49315f5d8b43398d3eca2c
Mayoor Rao <mayoor.rao@gmail.com>
2018-10-16 09:50:51 -0700

    app react

---
+++ precache-manifest.acec5de4685a6e4078f03f716d0807e1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f93be799dc7c59a02cf9",
+    "url": "/static/js/main.f93be799.chunk.js"
+  },
+  {
+    "revision": "3387d641ac36717196a8",
+    "url": "/static/js/1.3387d641.chunk.js"
+  },
+  {
+    "revision": "f93be799dc7c59a02cf9",
+    "url": "/static/css/main.4c8a8199.chunk.css"
+  },
+  {
+    "revision": "d737a40fad2f43bb05e9b720b21bac1b",
+    "url": "/index.html"
+  }
+];
